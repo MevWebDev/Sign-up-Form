729637333e25a811b6a3e8383fe614b678b568fe
Mev2137 <szymongrysiewicz@gmail.com>
2024-02-18 19:03:23 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let passwordInput = document.querySelector('#user-password');
+    passwordInput.addEventListener('input', function() {
+        let passwordValue = this.value;
+        console.log(passwordValue);
+        checkPassword();
+    });
+    let passwordInput2 = document.querySelector('#user-password2');
+    passwordInput2.addEventListener('input', function() {
+        let passwordValue2 = this.value;
+        checkPassword();
+        console.log(passwordValue2);
+    });
+    
+});
+function checkPassword(){
+    let passwordInput = document.querySelector('#user-password');
+    let passwordInput2 = document.querySelector('#user-password2');
+    let passwordValue = document.querySelector('#user-password').value;
+    let passwordValue2 = document.querySelector('#user-password2').value;
+    if(passwordValue === passwordValue2){
+        passwordInput.style.border = '1px solid green';
+        passwordInput2.style.border = '1px solid green';
+    }
+    else{
+        passwordInput.style.border = '1px solid red';
+        passwordInput2.style.border = '1px solid red';
+    }
+}
